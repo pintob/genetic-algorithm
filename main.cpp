d4c3f6f2a9ab572253cd77da3075f3119e2aa2a3
pintob <45038208+pintob@users.noreply.github.com>
2019-11-27 22:18:59 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include "Template/SortedArray.h"
+#include "include/Evolution.h"
+#include "include/Population.h"
+
+#include <iostream>
+
+using namespace genAlgo;
+
+int main() {
+    auto array = SortedArray(12);
+    Evolution evo = {64,25,4};
+    Population pop = Population<SortedArray>(array, evo, TypeOfProblem::Maximisation);
+
+   for(int i = 0; i < 10; i++){
+       ++pop;
+   }
+
+   std::cout << *pop[0].first << '\n';
+   pop+=100;
+   std::cout << *pop[0].first << '\n';
+
+   return 0;
+}
